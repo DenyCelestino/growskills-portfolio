a99da90efc8415adb45ef6450a11e482746ff323
Deny Celestino <denycelestino21@gmail.com>
2024-03-04 14:57:47 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Dolphin from "./images/dolphin.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,67 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="body-gradient"></div>
+        <div id="site-container">
+          <header>
+            <a className="header-logo" href="#"></a>
+          </header>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider">
+              <span className="slider-play"></span>
+              <span className="slider-pause"></span>
+            </span>
+          </label>
+          <div id="content-container">
+            <div className="content-grid">{children}</div>
+          </div>
+          <div className="dolphin">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <Image src={Dolphin} width={500} height={300} alt="" />
+        </div>
+        <div className="noise"></div>
+
+      <footer>
+        {/* <!-- <video src="video/turtle-output.mp4" autoplay muted loop></video> --> */}
+      </footer>
+      {/* <!-- end site-container --> */}
+
+      <div className="bubbles">
+        <div className="bubble bubble-1"></div>
+        <div className="bubble bubble-2"></div>
+        <div className="bubble bubble-3"></div>
+        <div className="bubble bubble-4"></div>
+        <div className="bubble bubble-5"></div>
+        <div className="bubble bubble-6"></div>
+        <div className="bubble bubble-7"></div>
+        <div className="bubble bubble-8"></div>
+        <div className="bubble bubble-9"></div>
+        <div className="bubble bubble-10"></div>
+        <div className="bubble bubble-11"></div>
+        <div className="bubble bubble-12"></div>
+        <div className="bubble bubble-13"></div>
+        <div className="bubble bubble-14"></div>
+        <div className="bubble bubble-15"></div>
+        <div className="bubble bubble-16"></div>
+        <div className="bubble bubble-17"></div>
+        <div className="bubble bubble-18"></div>
+        <div className="bubble bubble-19"></div>
+        <div className="bubble bubble-20"></div>
+        <div className="bubble bubble-21"></div>
+        <div className="bubble bubble-22"></div>
+        <div className="bubble bubble-23"></div>
+        <div className="bubble bubble-24"></div>
+        <div className="bubble bubble-25"></div>
+        <div className="bubble bubble-26"></div>
+        <div className="bubble bubble-27"></div>
+        <div className="bubble bubble-28"></div>
+        <div className="bubble bubble-29"></div>
+        <div className="bubble bubble-30"></div>
+      </div>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ app/projects/[name]/page.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+import Projects from "../../content.json";
+import Link from "next/link";
+import Dolphin from "../../images/dolphin.png";
+import Image from "next/image";
+const Project = ({ params }: { params: { name: string } }) => {
+  const name = decodeURI(params.name);
+
+  const data = Projects.find((item) => item.url === name);
+  return (
+    <>
+      <div className="project-img cover-image fullwidth">
+        <img src={data?.image.url} alt={data?.title} />
+        <div className="project-img-bg"></div>
+      </div>
+      <section className="hero-title">
+        <h1>{data?.title}</h1>
+      </section>
+      <section className="project-item with-margin">
+        <div className="project-item-image">
+          <img src={"/ipad.png"} alt={"ipad"} />
+          {data?.images.map((item, index) => (
+            <div className="project-item-image-image cover-image" key={index}>
+              <img src={item.url} alt={item.url} />
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="project-item-text with-margin text-container">
+        <div className="project-item-text-title">
+          {data?.text?.map((item, index) => (
+            <h3 key={index}>{item.title}</h3>
+          ))}
+        </div>
+        <div className="project-item-text-text">
+          {data?.text?.map((item, index) => (
+            <div key={index}>
+              <p key={index}>{item.text}</p>
+
+              {item.url && <Link href={item?.url}>{item?.urlLabel}</Link>}
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Project;
